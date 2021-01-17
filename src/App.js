e15c193d6eabc22cfe83986b13b4123cb18ac8dd
Shubhank Gaba <shubhankgaba92@gmail.com>
2021-01-17 20:17:01 +0530

    replaced class component using hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from 'react';
+// CODE LOOKS LIKE THIS BECASUE WE'RE USING A HOOKS
+
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import SearchBox from './components/searchBox.js';
 import CardList from "./components/cardList";
 import tachyons from 'tachyons';
 
+function App() {
+  const [searchField, setSearchField] = useState('');
+  const [pokemon, setPokemon] = useState([]);
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      searchField: '',
-      pokemon: []
-    }  
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
     .then(res => res.json())
     .then(data => {
@@ -28,33 +24,100 @@ class App extends React.Component {
           return KO
       })
       Promise.all(SinglePokemonData).then(data => {
-        this.setState({pokemon: data})
+        setPokemon(data)
       }).catch(err => console.log("Couldn't set the single pokemon data to the state array", err))
     })
     .catch(err => console.log("Error", err))
+  }, [])
+
+  const onSearchChange = (e) => {
+    setSearchField(e.target.value)
   }
 
-  onSearchChange = (e) => {
-    this.setState( { searchField: e.target.value } )
-  }
-
-  render() {
-    const { pokemon, searchField} = this.state;
-    const filteredPoke = pokemon.filter(pok => {
+  const filteredPoke = pokemon.filter(pok => {
       return pok.name.toLowerCase().includes(searchField);
-    })
+  })
 
+  if (filteredPoke.length !== 0) {
+    return (
+        <div className='tc r'>
+          <img src='https://fontmeme.com/permalink/201229/34cc001b3b4253ce0d2453c2b4c63937.png' style={{paddingTop: '10px'}}></img>
+          <SearchBox searchChange={onSearchChange} />
+          <CardList data={filteredPoke} />
+        </div>
+    )
+  } else {
     return (
       <div className='tc r'>
         <img src='https://fontmeme.com/permalink/201229/34cc001b3b4253ce0d2453c2b4c63937.png' style={{paddingTop: '10px'}}></img>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList data={filteredPoke} />
+        <SearchBox searchChange={onSearchChange} />
+        <h3>No pokemon found for the following Search. Please try again!</h3>
       </div>
-    )
+  )
   }
+
 }
 
 export default App
+
+// CODE WOULD'VE LOOKED LIKE THIS IF WE WERE USING A CLASS COMPONENT
+// import React, {Component, useEffect, useState} from 'react';
+// import './App.css';
+// import SearchBox from './components/searchBox.js';
+// import CardList from "./components/cardList";
+// import tachyons from 'tachyons';
+
+
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       searchField: '',
+//       pokemon: []
+//     }  
+//   }
+
+//   componentDidMount() {
+//     fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+//     .then(res => res.json())
+//     .then(data => {
+//       return data.results
+//     })
+//     .then(data => {
+//       let SinglePokemonData = data.map((user, i) => {
+//           const KO = fetch(data[i].url).then(res=> res.json()).then(data => {
+//             return data
+//           }).catch(err => console.log("Unable to get data from single pokemon data", err))
+//           return KO
+//       })
+//       Promise.all(SinglePokemonData).then(data => {
+//         this.setState({pokemon: data})
+//       }).catch(err => console.log("Couldn't set the single pokemon data to the state array", err))
+//     })
+//     .catch(err => console.log("Error", err))
+//   }
+
+//   onSearchChange = (e) => {
+//     this.setState( { searchField: e.target.value } )
+//   }
+
+//   render() {
+//     const { pokemon, searchField} = this.state;
+//     const filteredPoke = pokemon.filter(pok => {
+//       return pok.name.toLowerCase().includes(searchField);
+//     })
+
+//     return (
+//       <div className='tc r'>
+//         <img src='https://fontmeme.com/permalink/201229/34cc001b3b4253ce0d2453c2b4c63937.png' style={{paddingTop: '10px'}}></img>
+//         <SearchBox searchChange={this.onSearchChange} />
+//         <CardList data={filteredPoke} />
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
 
 
 
