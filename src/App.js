b67d41c8205ea213d0ff3e0252654949528b3259
unknown <shubhankgaba92@gmail.com>
2022-02-19 20:34:44 +0530

    removed useless code

--- src/App.js
+++ src/App.js
@@ -1,71 +1,8 @@
-// CODE LOOKS LIKE THIS BECASUE WE'RE USING A HOOKS
-
-// import React, { useEffect, useState} from 'react';
-// import './App.css';
-// import SearchBox from './components/searchBox.js';
-// import CardList from "./components/cardList";
-// import tachyons from 'tachyons';
-
-// function App() {
-//   const [searchField, setSearchField] = useState('');
-//   const [pokemon, setPokemon] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
-//     .then(res => res.json())
-//     .then(data => {
-//       return data.results
-//     }) 
-//     .then(data => {
-//       let SinglePokemonData = data.map((user, i) => {
-//           const KO = fetch(data[i].url).then(res=> res.json()).then(data => {
-//             return data
-//           }).catch(err => console.log("Unable to get data from single pokemon data", err))
-//           return KO
-//       })
-//       Promise.all(SinglePokemonData).then(data => {
-//         setPokemon(data)
-//       }).catch(err => console.log("Couldn't set the single pokemon data to the state array", err))
-//     })
-//     .catch(err => console.log("Error", err))
-//   }, [])
-
-//   const onSearchChange = (e) => {
-//     setSearchField(e.target.value)
-//   }
-
-//   const filteredPoke = pokemon.filter(pok => {
-//       return pok.name.toLowerCase().includes(searchField);
-//   })
-
-//   if (filteredPoke.length !== 0) {
-//     return (
-//         <div className='tc r'>
-//           <img src='https://fontmeme.com/permalink/201229/34cc001b3b4253ce0d2453c2b4c63937.png' style={{paddingTop: '10px'}}></img>
-//           <SearchBox searchChange={onSearchChange} />
-//           <CardList data={filteredPoke} />
-//         </div>
-//     )
-//   } else {
-//     return (
-//       <div className='tc r'>
-//         <img src='https://fontmeme.com/permalink/201229/34cc001b3b4253ce0d2453c2b4c63937.png' style={{paddingTop: '10px'}}></img>
-//         <SearchBox searchChange={onSearchChange} />
-//         <h3>No pokemon found for the following Search. Please try again!</h3>
-//       </div>
-//   )
-//   }
-
-// }
-
-// export default App
-
-// CODE WOULD'VE LOOKED LIKE THIS IF WE WERE USING A CLASS COMPONENT
-
-
 import React, {Component} from 'react';
 import ReactLoading from 'react-loading';
 import './App.css';
+
+//Components
 import SearchBox from './components/searchBox.js';
 import TypeList from "./components/typeList";
 import CardList from "./components/cardList";
